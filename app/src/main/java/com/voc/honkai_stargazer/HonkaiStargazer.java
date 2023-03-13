4fac4eba4c52ee25c11b4c0022c5ec75973a344a
khng1 <khng101@gmail.com>
2023-03-13 11:46:20 -0500

    Added dependencies. Specifically implementation of Dagger Hilt

---
+++ app/src/main/java/com/voc/honkai_stargazer/HonkaiStargazer.java
@@ -0,0 +1,9 @@
+package com.voc.honkai_stargazer;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class HonkaiStargazer extends Application {
+}
