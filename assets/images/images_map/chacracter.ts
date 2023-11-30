9b52ea37c681027eeb70827477213f863fcb0af6
Voc-夜芷冰 <voc.app.programmer@gmail.com>
2023-11-30 18:44:15 +0800

    feat: dummy imageFull (Splash first)

--- assets/images/images_map/chacracter.ts
+++ assets/images/images_map/chacracter.ts
@@ -1,116 +1,152 @@
 const Character = {
   Argenti: {
     icon: require("../../images/argenti_icon.webp"),
+    imageFull: require("../../images/argenti_splash.webp"),
   },
   Huohuo: {
     icon: require("../../images/huohuo_icon.webp"),
-  },
+    imageFull: require("../../images/huohuo_splash.webp"),
+	},
   Hanya: {
     icon: require("../../images/hanya_icon.webp"),
-  },
+    imageFull: require("../../images/hanya_splash.webp"),
+	},
   Jingliu: {
     icon: require("../../images/jingliu_icon.webp"),
-  },
+    imageFull: require("../../images/jingliu_splash.webp"),
+	},
   "Topaz & Numby": {
     icon: require("../../images/topaz_icon.webp"),
-  },
+    imageFull: require("../../images/topaz_splash.webp"),
+	},
   Guinaifen: {
     icon: require("../../images/guinaifen_icon.webp"),
-  },
+    imageFull: require("../../images/guinaifen_splash.webp"),
+	},
   "Dan Heng • Imbibitor Lunae": {
     icon: require("../../images/dan_heng_il_icon.webp"),
-  },
+		imageFull: require("../../images/dan_heng_il_splash.webp"),
+	},
   "Fu Xuan": {
     icon: require("../../images/fu_xuan_icon.webp"),
-  },
+		imageFull: require("../../images/fu_xuan_splash.webp"),
+	},
   Lynx: {
     icon: require("../../images/lynx_icon.webp"),
-  },
+		imageFull: require("../../images/lynx_splash.webp"),
+	},
   Kafka: {
     icon: require("../../images/kafka_icon.webp"),
-  },
+		imageFull: require("../../images/kafka_splash.webp"),
+	},
   Blade: {
     icon: require("../../images/blade_icon.webp"),
-  },
+		imageFull: require("../../images/blade_splash.webp"),
+	},
   Luka: {
     icon: require("../../images/luka_icon.webp"),
-  },
+		imageFull: require("../../images/luka_splash.webp"),
+	},
   Yukong: {
     icon: require("../../images/yukong_icon.webp"),
-  },
+		imageFull: require("../../images/yukong_splash.webp"),
+	},
   Luocha: {
     icon: require("../../images/luocha_icon.webp"),
-  },
+		imageFull: require("../../images/luocha_splash.webp"),
+	},
   "Silver Wolf": {
     icon: require("../../images/silver_wolf_icon.webp"),
-  },
+		imageFull: require("../../images/silver_wolf_splash.webp"),
+	},
   Arlan: {
     icon: require("../../images/arlan_icon.webp"),
-  },
+		imageFull: require("../../images/arlan_splash.webp"),
+	},
   Asta: {
     icon: require("../../images/asta_icon.webp"),
-  },
+		imageFull: require("../../images/asta_splash.webp"),
+	},
   Bailu: {
     icon: require("../../images/bailu_icon.webp"),
-  },
+		imageFull: require("../../images/bailu_splash.webp"),
+	},
   Bronya: {
     icon: require("../../images/bronya_icon.webp"),
-  },
+		imageFull: require("../../images/bronya_splash.webp"),
+	},
   Clara: {
     icon: require("../../images/clara_icon.webp"),
-  },
+		imageFull: require("../../images/clara_splash.webp"),
+	},
   "Dan Heng": {
     icon: require("../../images/dan_heng_icon.webp"),
-  },
+		imageFull: require("../../images/dan_heng_splash.webp"),
+	},
   Gepard: {
     icon: require("../../images/gepard_icon.webp"),
-  },
+		imageFull: require("../../images/gepard_splash.webp"),
+	},
   Herta: {
     icon: require("../../images/herta_icon.webp"),
-  },
+		imageFull: require("../../images/herta_splash.webp"),
+	},
   Himeko: {
     icon: require("../../images/himeko_icon.webp"),
-  },
+		imageFull: require("../../images/himeko_splash.webp"),
+	},
   Hook: {
     icon: require("../../images/hook_icon.webp"),
-  },
+		imageFull: require("../../images/hook_splash.webp"),
+	},
   "Jing Yuan": {
     icon: require("../../images/jing_yuan_icon.webp"),
-  },
+		imageFull: require("../../images/jing_yuan_splash.webp"),
+	},
   "March 7th": {
     icon: require("../../images/march_7th_icon.webp"),
-  },
+		imageFull: require("../../images/march_7th_splash.webp"),
+	},
   Natasha: {
     icon: require("../../images/natasha_icon.webp"),
-  },
+		imageFull: require("../../images/natasha_splash.webp"),
+	},
   Pela: {
     icon: require("../../images/pela_icon.webp"),
-  },
+		imageFull: require("../../images/pela_splash.webp"),
+	},
   Qingque: {
     icon: require("../../images/qingque_icon.webp"),
-  },
+		imageFull: require("../../images/qingque_splash.webp"),
+	},
   Sampo: {
     icon: require("../../images/sampo_icon.webp"),
-  },
+		imageFull: require("../../images/sampo_splash.webp"),
+	},
   Seele: {
     icon: require("../../images/seele_icon.webp"),
     imageFull: require("../../images/seele_full_image.png"),
   },
   Serval: {
     icon: require("../../images/serval_icon.webp"),
-  },
+		imageFull: require("../../images/serval_splash.webp"),
+	},
   Sushang: {
     icon: require("../../images/sushang_icon.webp"),
-  },
+		imageFull: require("../../images/sushang_splash.webp"),
+	},
   Tingyun: {
     icon: require("../../images/tingyun_icon.webp"),
-  },
+		imageFull: require("../../images/tingyun_splash.webp"),
+	},
   Welt: {
     icon: require("../../images/welt_icon.webp"),
+		imageFull: require("../../images/welt_icon.webp"),
   },
   Yanqing: {
     icon: require("../../images/yanqing_icon.webp"),
-  },
+		imageFull: require("../../images/yanqing_splash.webp"),
+	},
 };
 
 export default Character;
