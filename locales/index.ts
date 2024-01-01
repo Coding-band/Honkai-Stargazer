efbcc3109d42820d5ba0f2dc467cf2112fcb7021
Voc-夜芷冰 <voc.app.programmer@gmail.com>
2024-01-01 19:15:06 +0800

    feat : 新增EN, ZH_CN, VOCCHINESE 語言翻譯檔案

--- locales/index.ts
+++ locales/index.ts
@@ -1,9 +1,11 @@
 import { EN } from "./en";
 import { ZH_CN } from "./zh_cn";
 import { ZH_HK } from "./zh_hk";
+import { VOCCHINESE } from "./vocchinese";
 
 export const LOCALES =  {
   en: EN,
   zh_cn: ZH_CN,
   zh_hk: ZH_HK,
+  vocchinese : VOCCHINESE,
 };
