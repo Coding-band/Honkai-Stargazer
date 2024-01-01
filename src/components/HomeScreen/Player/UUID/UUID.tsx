9b843f26e038e92f78b7e49f3e3ba6beafcd3e3d
Dalufishe <fishawe87@gmail.com>
2024-01-01 13:40:32 +0800

    feat: UUID 複製功能

--- src/components/HomeScreen/Player/UUID/UUID.tsx
+++ src/components/HomeScreen/Player/UUID/UUID.tsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import useHsrUUID from "../../../../hooks/hoyolab/useHsrUUID";
+import Toast from "../../../../utils/toast/Toast";
+import * as Clipboard from "expo-clipboard";
 
 export default function UUID() {
   const uuid = useHsrUUID();
 
+  const handleCopyUUID = useCallback(async () => {
+    try {
+      await Clipboard.setStringAsync(uuid);
+      Toast("已複製到剪貼簿");
+    } catch (e) {
+      Toast("複製失敗, 錯誤代碼：" + e);
+    }
+  }, [uuid]);
+
   return (
-    <View
-      className="ml-[-4px] p-2 rounded-[50px]"
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "rgba(0, 0, 0, 0.3)",
-      }}
-    >
-      <Text className="text-white">{uuid || "00000000"}</Text>
-    </View>
+    <TouchableOpacity activeOpacity={0.35} onPress={handleCopyUUID}>
+      <View
+        className="ml-[-4px] p-2 rounded-[50px]"
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(0, 0, 0, 0.3)",
+        }}
+      >
+        <Text className="text-white">{uuid || "00000000"}</Text>
+      </View>
+    </TouchableOpacity>
   );
 }
