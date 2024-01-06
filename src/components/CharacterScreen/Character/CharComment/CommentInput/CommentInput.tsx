5021e99529da30e9bc7adfca20fb42ff2f79e4a0
Dalufishe <fishawe87@gmail.com>
2024-01-06 14:03:18 +0800

    hot fix

--- src/components/CharacterScreen/Character/CharComment/CommentInput/CommentInput.tsx
+++ src/components/CharacterScreen/Character/CharComment/CommentInput/CommentInput.tsx
@@ -16,17 +16,49 @@ import officalCharId from "../../../../../../map/character_offical_id_map";
 import useCharComments from "../../../../../firebase/hooks/useCharComments";
 import { BlurView } from "expo-blur";
 import useFirebaseUid from "../../../../../firebase/hooks/useFirebaseUid";
-
-
+import Toast from "../../../../../utils/toast/Toast";
 
 export default function CommentInput() {
-  const [input, setInput] = useState("");
-
   const uid = useFirebaseUid();
   const charId = useCharId();
   const officalId = findKey(officalCharId, (v) => v === charId);
 
+  const [input, setInput] = useState("");
   const { refetch } = useCharComments(officalId || "");
+
+  const handleSubmit = async () => {
+    if (uid) {
+      const isDocExists = (await db.CharacterComments.doc(officalId).get())
+        .exists;
+
+      try {
+        if (isDocExists) {
+          await db.CharacterComments.doc(officalId).update({
+            comments: firestore.FieldValue.arrayUnion({
+              user_id: uid,
+              content: input,
+            }),
+          });
+        } else {
+          await db.CharacterComments.doc(officalId).set({
+            comments: firestore.FieldValue.arrayUnion({
+              user_id: uid,
+              content: input,
+            }),
+          });
+        }
+
+        setTimeout(() => {
+          Toast("留言成功！");
+          refetch();
+        }, 500);
+      } catch (e) {
+        Toast("留言失敗，錯誤訊息：" + e.message);
+      }
+
+      setInput("");
+    }
+  };
 
   return (
     <View className="w-full h-[80px] " style={{ justifyContent: "center" }}>
@@ -41,33 +73,7 @@ export default function CommentInput() {
             style={{ justifyContent: "center" }}
             value={input}
             onChangeText={setInput}
-            onSubmitEditing={async () => {
-              if (uid) {
-                const docExists = (
-                  await db.CharacterComments.doc(officalId).get()
-                ).exists;
-
-                if (docExists) {
-                  await db.CharacterComments.doc(officalId).update({
-                    comments: firestore.FieldValue.arrayUnion({
-                      user_id: uid,
-                      content: input,
-                    }),
-                  });
-                  refetch();
-                } else {
-                  await db.CharacterComments.doc(officalId).set({
-                    comments: firestore.FieldValue.arrayUnion({
-                      user_id: uid,
-                      content: input,
-                    }),
-                  });
-                  refetch();
-                }
-
-                setInput("");
-              }
-            }}
+            onSubmitEditing={handleSubmit}
             placeholder="幫幫我 史瓦羅先生！"
             placeholderTextColor="gray"
           />
