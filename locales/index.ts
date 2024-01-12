3ab6c212782660894bdd1c5713e8ade1c0464c90
Voc-夜芷冰 <voc.app.programmer@gmail.com>
2024-01-12 19:06:10 +0800

    fix : 穹 ...
    大家都窮了 體諒一下吧

--- locales/index.ts
+++ locales/index.ts
@@ -3,6 +3,19 @@ import { strings as ZH_CN} from "./zh-CN/strings";
 import { strings as ZH_HK} from "./zh-HK/strings";
 import { strings as VOCCHINESE } from "./yue/strings";
 import { strings as JP } from "./ja/strings";
+/*
+import { strings as ES } from "./es-ES/strings";
+import { strings as DE } from "./de/strings";
+import { strings as FR } from "./fr/strings";
+import { strings as ID } from "./id/strings";
+import { strings as KR } from "./ko/strings";
+import { strings as PT } from "./pt-PT/strings";
+import { strings as RU } from "./ru/strings";
+import { strings as TH } from "./th/strings";
+import { strings as VI } from "./vi/strings";
+
+ NOT IN USED NOW
+*/
 
 export const LOCALES =  {
   en: EN,
