feb963e42394e6acfcc48e848024cbc8c99fb9e5
Voc-夜芷冰 <voc.app.programmer@gmail.com>
2024-02-20 23:41:41 +0800

    update : 抽卡算法（整理版本、未套入使用）

--- src/utils/lottery/LotterySimulator.js
+++ src/utils/lottery/LotterySimulator.js
@@ -3,6 +3,7 @@ const PULL_LIMIT_CHAR = "LIMIT_CHAR" //限定角色池
 const PULL_LIMIT_LIGHTCONE = "LIMIT_LC" //限定光錐池
 const PULL_STATIC = "STATIC" //常駐池
 
+//三星常駐
 const itemRare3 = [
     "鋒鏑","物穰","天傾","琥珀",
     "幽邃","齊頌","智庫","離弦",
@@ -12,6 +13,7 @@ const itemRare3 = [
     "睿見"
 ]
 
+//四星光錐常駐
 const itemRare4LC = [
     "一場術後對話","晚安與睡顏","餘生的第一天","唯有沉默",
     "記憶中的模樣","鼴鼠黨歡迎你","「我」的誕生","同一種心情",
@@ -21,6 +23,7 @@ const itemRare4LC = [
     "天才們的休憩",
 ]
 
+//四星角色常駐
 const itemRare4Char = [
     "素裳","黑塔","娜塔莎","希露瓦",
     "佩拉","桑博","虎克","艾絲妲",
@@ -29,11 +32,33 @@ const itemRare4Char = [
     "三月七"
 ]
 
-
-const itemRare5 = [
+//五星角色常駐
+const itemRare5Char = [
     "姬子","瓦爾特","布洛妮婭","傑帕德",
     "克拉拉","彥卿","白露"
 ]
+
+//五星光錐常駐
+const itemRare5LC = [
+    "姬子","瓦爾特","布洛妮婭","傑帕德",
+    "克拉拉","彥卿","白露"
+]
+
+//限定角色
+const pullNormalRateChar = [0.943, 0.051, 0.006]
+const pullPityRateChar = [0.854, 0.13, 0.016]
+const pullPityOrNotChar = 0.5
+const pullPityMaxChar = 90
+
+//限定光錐
+const pullNormalRateLC = [0.943, 0.051, 0.006]
+const pullPityRateLC = [0.854, 0.13, 0.016]
+const pullPityOrNotLC = 0.75
+const pullPityMaxLC = 80
+
+//常駐
+const pullNormalRateStatic = [0.943, 0.051, 0.006]
+const pullPityRateStatic = [0.854, 0.13, 0.016]
 
 const headerRare3n = "\x1b[42m【3✦常駐】\x1b[0m"
 const headerRare4n = "\x1b[45m【4✦常駐】\x1b[0m"
@@ -48,61 +73,60 @@ function makePulls(pullInfo, pullType, pullCount){
 
 function testPulls(){
     let pullArray = []
-    const pullNormalRate = [0.943, 0.051, 0.006]
-    const pullPityRate = [0.854, 0.13, 0.016]
+    let pullNormalRate = pullNormalRateChar
+    let pullPityRate = pullPityRateChar
     let pullRate = pullNormalRate
+    let pullPityOrNot = pullPityOrNotChar
+    let pullNormalSet = itemRare3
+    let pullUpSet = itemRare3
+    let pullPityMax = pullPityMaxChar
     let pullsAfterRare4 = 0
     let pullsAfterRare5 = 0
     let isMustGetRare5  = false
     let isMustGetRare4  = false
+    let pulledItemType = "THREE"
 
     const itemUpRare4 = ["桂乃芬","米沙","停雲"]
     const itemUpRare5 = ["黑天鵝"]
 
     for(let x = 0 ; x < 180 ; x++){
         const randNum = Math.random(Date.now())
-        
-        if(randNum <= pullRate[0] && pullsAfterRare4 < 9 && pullsAfterRare5 < 89){
-            //3星
-            const randNumRare3 = Math.trunc(Math.random(Date.now()) * itemRare3.length);
-            pullArray.push(headerRare3n+itemRare3[randNumRare3])
-
-            pullsAfterRare4 ++
-            pullsAfterRare5 ++
-        }else if (randNum <= pullRate[0] + pullRate[1] && pullsAfterRare5 < 89){
-            //4星
-            const randNum4s = Math.random(Date.now())
-            if(randNum4s < 0.5 && !isMustGetRare4){
-                const randNumRare4 = Math.trunc(Math.random(Date.now()) * (itemRare4Char.length + itemRare4LC.length));
-                pullArray.push(headerRare4n+(randNumRare4 < itemRare4Char.length ? itemRare4Char[randNumRare4] : itemRare4LC[randNumRare4-itemRare4Char.length]))
-                isMustGetRare4 = true
-            }else{
-                const randNumRare4 = Math.trunc(Math.random(Date.now()) * itemUpRare4.length);
-                pullArray.push(headerRare4u+itemUpRare4[randNumRare4])
-                isMustGetRare4 = false
-            }
-
-            pullsAfterRare4 = 0
-            pullsAfterRare5 ++
+        //75 + Math.random(Date.now())* 15 不應該出現 但爲了避免過高機率是89
+        if(randNum <= pullRate[0] && pullsAfterRare4 < 9 && pullsAfterRare5 < pullPityMax - 15 + Math.random(Date.now())* 15 ){
+            pulledItemType = "THREE";
+            pullNormalSet = itemRare3
+            pullUpSet = itemRare3
+        }else if(randNum <= pullRate[0] + pullRate[1] && pullsAfterRare5 < pullPityMax - 15 + Math.random(Date.now())* 15){
+            pulledItemType = "FOUR";
+            pullNormalSet = itemRare4Char.concat(itemRare4LC)
+            pullUpSet = itemUpRare4
+            isMustGetRare4 = !isMustGetRare4
         }else{
-            //5星
-            const randNum5s = Math.random(Date.now())
-            if(randNum5s < 0.5 && !isMustGetRare5){
-                //歪咗
-                const randNumRare5 = Math.trunc(Math.random(Date.now()) * (itemRare5.length));
-                pullArray.push(headerRare5n+itemRare5[randNumRare5])
-                pullRate = pullPityRate
-                isMustGetRare5 = true
-            }else{
-                //冇歪
-                pullArray.push(headerRare5u+itemUpRare5[0])
-                isMustGetRare5 = false
-            }
-
-            pullsAfterRare4 ++
-            pullsAfterRare5 = 0
+            pulledItemType = "FIVE";
+            pullNormalSet = itemRare5Char
+            pullUpSet = itemUpRare5
+            isMustGetRare5 = !isMustGetRare5
+            pullRate = (isMustGetRare5 ? pullPityRate : pullNormalRate)
         }
+
+        (pulledItemType === "FIVE" ? console.log("距離上一次5星 "+pullsAfterRare5) : "")
+
+        pullsAfterRare4 = (pulledItemType === "FOUR" ? 0 : pullsAfterRare4 + 1)
+        pullsAfterRare5 = (pulledItemType === "FIVE" ? 0 : pullsAfterRare5 + 1)
+        
+        const randPityOrNot = Math.random(Date.now())
+        const randIndex = Math.trunc(Math.random(Date.now()) * (
+            (isMustGetRare4 && pulledItemType === "FOUR" || isMustGetRare5 && pulledItemType === "FIVE" ? pullUpSet.length : (randPityOrNot < pullPityOrNot ? pullNormalSet.length : pullUpSet.length))
+        ));
+
+        pullArray.push((
+                pulledItemType === "THREE" && true ? headerRare3n
+                : (pulledItemType === "FOUR") ? ((isMustGetRare4) ? headerRare4u : (randPityOrNot < pullPityOrNot ? headerRare4n : headerRare4u)) 
+                : ((isMustGetRare5) ? headerRare5u : (randPityOrNot < pullPityOrNot ? headerRare5n : headerRare5u)) 
+            ) + ((randPityOrNot < pullPityOrNot) ? ((isMustGetRare4 && pulledItemType === "FOUR" || isMustGetRare5 && pulledItemType === "FIVE") ? pullUpSet : pullNormalSet) : pullUpSet)[randIndex]
+        )
     }
+    
     for(let c = 0 ; c < pullArray.length ; c++){
         console.log((c < 100 ? "0" : "") + (c < 10 ? "0" : "") + c +"\t"+pullArray[c])
     }
