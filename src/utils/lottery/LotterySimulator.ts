2f8727b7ced1bd91c8a02f6c34a96cf25ceea168
Voc-夜芷冰 <voc.app.programmer@gmail.com>
2024-03-01 18:25:04 +0800

    update : 抽卡模擬常數陣列英文化
    不然沒法正確被讀取 & 日後方便JSON設定

--- src/utils/lottery/LotterySimulator.ts
+++ src/utils/lottery/LotterySimulator.ts
@@ -1,43 +1,74 @@
 import util from "util"
 //三星常駐
-export const itemRare3 = [
+/**
     "鋒鏑","物穰","天傾","琥珀",
     "幽邃","齊頌","智庫","離弦",
     "嘉果","樂圮","戍禦","輪契",
     "靈鑰","相抗","俱歿","開疆",
     "匿影","淵環","蕃息","調和",
     "睿見"
+ */
+export const itemRare3 = [
+    "Arrows","Cornucopia","Collapsing Sky","Amber",
+    "Void","Chorus","Data Bank","Darting Arrow",
+    "Fine Fruit","Shattered Home","Defense","Meshing Cogs",
+    "Passkey","Adversarial","Mutual Demise","Pioneering",
+    "Hidden Shadow","Loop","Multiplication","Mediation",
+    "Sagacity"
 ]
 
 //四星光錐常駐
-export const itemRare4LC = [
-    "一場術後對話","晚安與睡顏","餘生的第一天","唯有沉默",
+/**
+   "一場術後對話","晚安與睡顏","餘生的第一天","唯有沉默",
     "記憶中的模樣","鼴鼠黨歡迎你","「我」的誕生","同一種心情",
     "獵物的視線","朗道的選擇","論劍","與行星相會",
     "秘密誓心","別讓世界靜下來","此時恰好","決心如汗珠般閃耀",
     "宇宙市場趨勢","按個追蹤吧!","舞!舞!舞!","在藍天下",
     "天才們的休憩",
+ */
+export const itemRare4LC = [
+    "Post-Op Conversation","Good Night and Sleep Well","Day One of My New Life","Only Silence Remains",
+    "Memories of the Past","The Moles Welcome You","The Birth of the Self","Shared Feeling",
+    "Eyes of the Prey","Landau's Choice","Swordplay","Planetary Rendezvous",
+    "A Secret Vow","Make the World Clamor","Perfect Timing","Resolution Shines As Pearls of Sweat",
+    "Trend of the Universal Market","Subscribe for More!","Dance! Dance! Dance!","Under the Blue Sky",
+    "Geniuses' Repose",
 ]
 
 //四星角色常駐
-export const itemRare4Char = [
+/**
     "素裳","黑塔","娜塔莎","希露瓦",
     "佩拉","桑博","虎克","艾絲妲",
     "青雀","阿蘭","馭空","盧卡",
     "丹恆","寒鴉","雪衣","玲可",
     "三月七","桂乃芬","停雲",
+ */
+export const itemRare4Char = [
+    "Sushang","Herta","Natasha","Serval",
+    "Pela","Sampo","Hook","Asta",
+    "Qingque","Arlan","Yukong","Luka",
+    "Dan Heng","Hanya","Xueyi","Lynx",
+    "March 7th","Guinaifen","Tingyun",
 ]
 
 //五星角色常駐
-export const itemRare5Char = [
+/**
     "姬子","瓦爾特","布洛妮婭","傑帕德",
     "克拉拉","彥卿","白露"
+ */
+export const itemRare5Char = [
+    "Himeko","Welt","Bronya","Gepard",
+    "Clara","Yanqing","Bailu"
 ]
 
 //五星光錐常駐
-export const itemRare5LC = [
+/**
     "銀河鐵道之夜","無可取代的東西","但戰鬥還未結束","以世界之名",
     "制勝的瞬間","如泥酣眠","時節不居"
+ */
+export const itemRare5LC = [
+    "Night on the Milky Way","Something Irreplaceable","But the Battle Isn't Over","In the Name of the World",
+    "Moment of Victory","Sleep Like the Dead","Time Waits for No One"
 ]
 
 //限定角色
@@ -57,15 +88,20 @@ export const pullNormalRateStatic = [0.943, 0.051, 0.006]
 export const pullPityRateStatic = [0.854, 0.13, 0.016]
 
 export enum PullType{
-    PULL_LIMIT_CHAR,  //限定角色池
-    PULL_LIMIT_LIGHTCONE, //限定光錐池
-    PULL_STATIC //常駐池
+    PULL_LIMIT_CHAR = "CHAR",  //限定角色池
+    PULL_LIMIT_LIGHTCONE = "LIGHTCONE", //限定光錐池
+    PULL_STATIC = "STATIC" //常駐池
 }
 
 export class PullInfo{
-    special_rare5: Array<string> = [];
-    special_rare4: Array<string> = [];
-  };
+  version: string = "1.0";
+  phase: number = 1;
+  versionCode: number = 1001;
+  type: string = "STATIC";
+  special_rare5: Array<string> = [];
+  special_rare4: Array<string> = [];
+  title: object = {"en" : "常駐", "zh_hk" : "常駐"};
+};
 
   export class PullConfig{
     pullsAfterRare4 : number = 0;
@@ -81,7 +117,7 @@ export class PullInfo{
  * @param pullCount number (mainly will have 1 and 10 only.)
  * @returns 
  */
-export default function makePulls(pullInfo : PullInfo, pullConfig : PullConfig, pullType : PullType, pullCount : number) : object{
+export default function makePulls(pullInfo : PullInfo, pullConfig : PullConfig, pullCount : number) : object{
     /* 暫存本次抽卡的紀錄 : 
         [
             // 距離上次抽到五星後，過了78抽，第79抽保底抽到阮．梅
@@ -95,6 +131,7 @@ export default function makePulls(pullInfo : PullInfo, pullConfig : PullConfig, 
             ...
         ]
     */
+    let pullType = pullInfo.type as PullType
     let pullArray = []
 
     //抽卡基礎機率 & 抽卡保底機率
