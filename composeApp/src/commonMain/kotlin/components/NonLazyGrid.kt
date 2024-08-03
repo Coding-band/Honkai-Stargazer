dd910905dd24b4c9b17924d8f1cb40192084db79
Voc-夜芷冰 <voc.app.programmer@gmail.com>
2024-08-04 01:26:57 +0800

    feat: NonLazyGrid

---
+++ composeApp/src/commonMain/kotlin/components/NonLazyGrid.kt
@@ -0,0 +1,43 @@
+package components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+//https://dev.to/maaxgr/jetpack-compose-grid-without-lazy-5gb8
+@Composable
+fun NonLazyGrid(
+    columns: Int,
+    itemCount: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable() (Int) -> Unit
+) {
+    Column(modifier = modifier) {
+        var rows = (itemCount / columns)
+        if (itemCount.mod(columns) > 0) {
+            rows += 1
+        }
+
+        for (rowId in 0 until rows) {
+            val firstIndex = rowId * columns
+
+            Row {
+                for (columnId in 0 until columns) {
+                    val index = firstIndex + columnId
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f)
+                    ) {
+                        if (index < itemCount) {
+                            content(index)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
