a68498193d4ef8f69b3f94dbea1eb5e5bfb88ed4
Voc-夜芷冰 <voc.app.programmer@gmail.com>
2025-02-10 23:01:26 +0800

    feat: ActionOrderListPageScreen Add TeamListItem Builder

--- composeApp/src/commonMain/kotlin/ui/screens/ActionOrderListPageScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/screens/ActionOrderListPageScreen.kt
@@ -1,42 +1,80 @@
 package ui.screens
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.cheonjaeung.compose.grid.HorizontalGrid
 import com.cheonjaeung.compose.grid.SimpleGridCells
+import com.cheonjaeung.compose.grid.VerticalGrid
 import dev.chrisbanes.haze.HazeState
 import dev.chrisbanes.haze.haze
 import dev.chrisbanes.haze.hazeChild
+import files.AccountSetup
+import files.ActionOrderAddItem
+import files.ActionOrderItemChosen
 import files.Res
+import files.UserInfoOwnedCharacters
 import files.phorphos_check_regular
+import files.ui_icon_close
+import files.ui_icon_right
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDateTime
@@ -45,24 +83,41 @@ import kotlinx.datetime.format
 import kotlinx.datetime.format.byUnicodePattern
 import kotlinx.datetime.toLocalDateTime
 import moe.tlaster.precompose.navigation.Navigator
+import org.jetbrains.compose.resources.painterResource
+import types.Attribute
 import types.Character
 import types.CombatType
+import types.HsrProperties
 import types.Path
+import types.UserAccount
 import ui.components.BackIcon
 import ui.components.CharacterCard
 import ui.components.HeaderData
 import ui.components.PAGE_HEADER_HEIGHT
 import ui.components.PageHeader
+import ui.components.UIButton
+import ui.components.UIButtonSize
 import ui.components.defaultHeaderData
 import utils.annotation.DoItLater
 import utils.app.Constants
+import utils.app.Constants.Companion.CHAR_CARD_HEIGHT
+import utils.app.Constants.Companion.CHAR_CARD_TITLE_HEIGHT
+import utils.app.Constants.Companion.CHAR_CARD_WIDTH
 import utils.app.Constants.Companion.CLARA_KAMOJI
+import utils.app.Constants.Companion.INFO_MAX_WIDTH
+import utils.app.Constants.Companion.INFO_MIN_WIDTH
 import utils.app.Constants.Companion.SCREEN_SAVE_PADDING
+import utils.app.Constants.Companion.getCardBgColorByRare
+import utils.app.FontSizeNormal12
 import utils.app.FontSizeNormal14
 import utils.app.FontSizeNormal16
+import utils.app.FontSizeNormalLarge24
+import utils.app.TextColorNormalDim
+import utils.app.removeStrQuote
+import utils.hoyolab.AttributeExchange
 
 data class TeamListItem(
-    val uid: String,
+    val uid: String = UserAccount.INSTANCE.uid,
     val teamBuildUnix: Long = Clock.System.now().toEpochMilliseconds(),
     val id: String = "${uid}-${teamBuildUnix}",
     val teamName: String = CLARA_KAMOJI,
@@ -113,11 +168,19 @@ fun ActionOrderListPageScreen(
     val hazeState = remember { HazeState() }
     val itemList = remember { mutableStateListOf<TeamListItem>() }
     val isInit = remember { mutableStateOf(false) }
+    val isPopupOpen = remember { mutableStateOf(false) }
+    val localCharList = rememberSaveable { mutableStateOf<ArrayList<Character>>(arrayListOf()) }
 
     //Just Testing
     LaunchedEffect(Unit){
         if(!isInit.value){
             itemList.addAll(TEST_LIST)
+            localCharList.value.addAll(UserAccount.INSTANCE.characterList)
+            charList.value.forEach { char ->
+                if(UserAccount.INSTANCE.characterList.filter { it.officialId == char.officialId }.isEmpty()){
+                    localCharList.value.add(char)
+                }
+            }
             isInit.value = true
         }
     }
@@ -141,7 +204,7 @@ fun ActionOrderListPageScreen(
                 }
             }
             item {
-                Spacer(Modifier.padding(top = 16.dp).navigationBarsPadding())
+                Spacer(Modifier.height(64.dp).navigationBarsPadding())
             }
 
         }
@@ -156,7 +219,226 @@ fun ActionOrderListPageScreen(
                 //多選功能
             }
         )
+
+        //Button for 添加
+        UIButton(
+            textRes = Res.string.ActionOrderAddItem,
+            buttonSize = UIButtonSize.Normal,
+            modifierTmp = Modifier.align(Alignment.BottomCenter).padding(bottom = 16.dp).wrapContentWidth().widthIn(100.dp, 200.dp).wrapContentHeight(),
+            onClick = {
+                isPopupOpen.value = true
+            }
+        )
+
+        //I want the animation looks like a popup pull up from the bottom, with acceration and deceleration
+        AnimatedVisibility(
+            isPopupOpen.value,
+            enter = slideInVertically(
+                initialOffsetY = { it },
+                animationSpec = tween(
+                    durationMillis = 500,
+                    easing = FastOutSlowInEasing
+                )
+            ),
+            exit = slideOutVertically(
+                targetOffsetY = { it },
+                animationSpec = tween(
+                    durationMillis = 500,
+                    easing = FastOutSlowInEasing
+                )
+            ),
+            modifier = Modifier.widthIn(INFO_MIN_WIDTH, INFO_MAX_WIDTH).fillMaxHeight().align(Alignment.BottomCenter)
+        ){
+            Box(
+                modifier = Modifier.align(Alignment.Center)
+            ) {
+                TeamSelectPopup(itemList, localCharList, isPopupOpen = isPopupOpen)
+            }
+        }
     }
+
+
+}
+
+@Composable
+fun TeamSelectPopup(itemList: SnapshotStateList<TeamListItem>, localCharList: MutableState<ArrayList<Character>>,isPopupOpen: MutableState<Boolean>){
+    val teamDataList = remember { mutableStateListOf<TeammateItem>() }
+    //UI Part
+    Box(modifier = Modifier.fillMaxSize()){
+        val scrollableState = rememberScrollableState { 0f }
+        Column(modifier = Modifier.scrollable(
+            state = scrollableState, orientation = Orientation.Horizontal
+        )) {
+            //Padding for Status Bar
+            Spacer(Modifier
+                .height(0.dp)
+                .statusBarsPadding()
+            )
+
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .clip(shape = RoundedCornerShape(12.dp, 12.dp, 0.dp, 0.dp))
+                .background(Color(0xFF222222))
+                .padding(16.dp),
+            ) {
+                //Exit & Confirm Button
+                Row {
+                    //Exit Button
+                    Box(
+                        modifier = Modifier
+                            .size(48.dp)
+                            .clip(shape = CircleShape)
+                            .background(Color(0x33FFFFFF))
+                            .clickable { isPopupOpen.value = false }
+                    ) {
+                        Image(
+                            painter = painterResource(Res.drawable.ui_icon_close),
+                            contentDescription = "Exit Without Saving",
+                            modifier = Modifier.size(32.dp).align(Alignment.Center),
+                            colorFilter = ColorFilter.tint(Color.White)
+                        )
+                    }
+
+                    Spacer(Modifier.weight(1f))
+
+                    Box(
+                        modifier = Modifier
+                            .size(48.dp)
+                            .clip(shape = CircleShape)
+                            .background(Color(0x33FFFFFF))
+                            .clickable {
+                                val ret = arrayListOf<TeammateItem>()
+                                ret.addAll(teamDataList)
+                                itemList.add(TeamListItem(teamDataList = ret))
+                                isPopupOpen.value = false
+                            }
+                    ) {
+                        Image(
+                            painter = painterResource(Res.drawable.ui_icon_right),
+                            contentDescription = "Saving Button",
+                            modifier = Modifier.size(32.dp).align(Alignment.Center),
+                            colorFilter = ColorFilter.tint(Color.White)
+                        )
+                    }
+                }
+
+                Spacer(Modifier.height(16.dp))
+
+                //Select Teammates
+                LazyVerticalGrid(
+                    columns = GridCells.Adaptive(CHAR_CARD_WIDTH),
+                    horizontalArrangement = Arrangement.spacedBy(12.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                    modifier = Modifier.padding(
+                        start = SCREEN_SAVE_PADDING,
+                        end = SCREEN_SAVE_PADDING
+                    )
+                ){
+                    //Current Have Characters
+                    items(teamDataList) {
+                        CharacterCard(it.character, isDisplayLevel = true, onClick = {
+                            //Remove this character from teamList
+                            teamDataList.remove(it)
+                        })
+                    }
+
+                    //1,2,3,4
+                    for (i in teamDataList.size until 4){
+                        item {
+                            Box(
+                                modifier = Modifier
+                                    .widthIn(CHAR_CARD_WIDTH, CHAR_CARD_WIDTH *2)
+                                    .aspectRatio(CHAR_CARD_WIDTH / CHAR_CARD_HEIGHT)
+                                    .clip(
+                                        RoundedCornerShape(
+                                            topEnd = 15.dp,
+                                            topStart = 4.dp,
+                                            bottomEnd = 4.dp,
+                                            bottomStart = 4.dp
+                                        )
+                                    )
+                                    .background(Color(0xFF666666))
+                                    .clickable {  }
+                            ){
+                                Text(
+                                    text = "${i + 1}",
+                                    style = FontSizeNormalLarge24(),
+                                    color = Color(0xCCFFFFFF),
+                                    modifier = Modifier.align(Alignment.Center).wrapContentSize()
+                                )
+                            }
+                        }
+                    }
+                }
+
+                Spacer(Modifier.height(16.dp))
+
+                if(UserAccount.INSTANCE.uid !== "000000000"){
+                    Text(
+                        text = "${UserAccount.INSTANCE.username} (${UserAccount.INSTANCE.uid}): "+removeStrQuote(Res.string.UserInfoOwnedCharacters),
+                        style = FontSizeNormal16(),
+                        color = Color(0xCCFFFFFF),
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                    Spacer(Modifier.height(16.dp))
+
+                    //Owned Characters List
+
+                    val lazyGridState = rememberLazyGridState()
+                    LazyVerticalGrid(
+                        columns = GridCells.Adaptive(CHAR_CARD_WIDTH),
+                        horizontalArrangement = Arrangement.spacedBy(12.dp),
+                        verticalArrangement = Arrangement.spacedBy(12.dp),
+                        state = lazyGridState,
+                        modifier = Modifier.padding(
+                            start = SCREEN_SAVE_PADDING,
+                            end = SCREEN_SAVE_PADDING
+                        )
+                    ){
+                        items(localCharList.value){ it ->
+                            CharacterCard(
+                                isDisplayLevel = false,
+                                character = it,
+                                onClick = {
+                                    if(teamDataList.size < 4 && teamDataList.none { itTeam -> itTeam.character.officialId == it.officialId }){
+                                        teamDataList.add(
+                                            TeammateItem(
+                                                character = it,
+                                                level = it.characterStatus?.characterLevel ?: 1,
+                                                energyRechargeRate = getSpecificAttrFromChar(it, Attribute.ATTR_SP_RATE)?.valueFinal ?: 1f,
+                                                speedBase = getSpecificAttrFromChar(it, Attribute.ATTR_SPD)?.valueFinal ?: 0f,
+                                                speedRate = getSpecificAttrFromChar(it, Attribute.ATTR_SPD)?.valueAdd ?: 0f
+                                            )
+                                        )
+                                    }
+                                    println(teamDataList.size)
+                                },
+                                overrideNameComponent = if(teamDataList.filter { itTeam -> itTeam.character.officialId == it.officialId }.isEmpty()) {
+                                    null
+                                } else {
+                                    {
+                                        Text(
+                                            text = removeStrQuote(Res.string.ActionOrderItemChosen),
+                                            textAlign = TextAlign.Center,
+                                            color = TextColorNormalDim,
+                                            fontSize = FontSizeNormal12().fontSize,
+                                            lineHeight = CHAR_CARD_TITLE_HEIGHT.value.sp,
+                                            maxLines = 1
+                                        )
+                                    }
+                                }
+                            )
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+fun getSpecificAttrFromChar(char: Character, attr: Attribute) : HsrProperties? {
+    return char.characterStatus?.characterProperties?.find { it.attributeExchange.attribute == attr }
 }
 
 @DoItLater("Pack this style's card as a Common Component")
