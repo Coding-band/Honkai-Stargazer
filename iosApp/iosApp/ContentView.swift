2e91289983674fe02767898a4ae937ae10d10a93
Voc-夜芷冰 <voc.app.programmer@gmail.com>
2025-02-24 21:24:02 +0800

    feat: CMP Navigation Animation

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -4,7 +4,7 @@ import composeApp
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
-        MainViewControllerKt.MainViewController()
+        return MainViewControllerKt.MainViewController()
     }
 
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
