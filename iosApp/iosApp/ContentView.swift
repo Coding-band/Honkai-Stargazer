1d58a017e9b20cc7d258b97f9099f3af6be14028
Voc-夜芷冰 <voc.app.programmer@gmail.com>
2025-02-25 22:14:59 +0800

    fix: Upgrade Lib Version for Enable Swipe to GO Back

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -4,7 +4,7 @@ import composeApp
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
-        return MainViewControllerKt.MainViewController()
+        MainViewControllerKt.MainViewController()
     }
 
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
@@ -17,6 +17,7 @@ struct ContentView: View {
                 .ignoresSafeArea(edges: .all)
     }
 }
+
 
 
 
