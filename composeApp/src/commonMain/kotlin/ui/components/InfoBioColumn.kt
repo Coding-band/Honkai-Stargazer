4fb04c6c3d42e737a0a34c1748fdeda4c7ac7dfa
Voc-夜芷冰 <voc.app.programmer@gmail.com>
2025-03-07 19:26:08 +0800

    fix: Navigation Padding Of InfoBioColumn
    ref : https://discord.com/channels/880921456903618610/1347461178825117706

--- composeApp/src/commonMain/kotlin/ui/components/InfoBioColumn.kt
+++ composeApp/src/commonMain/kotlin/ui/components/InfoBioColumn.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -136,7 +137,7 @@ fun InfoBioColumn(
 
             Box(Modifier.height(6.dp))
 
-            Row {
+            Row(modifier = Modifier.navigationBarsPadding()) {
                 if (path !== null) {
                     Image(
                         painter = painterResource(path.iconWhite),
